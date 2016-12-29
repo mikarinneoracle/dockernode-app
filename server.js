73e4e431bcdeff2700dfce46a08d5374bb730ef2
Mika Rinne <mika.rinne@oracle.com>
2016-12-29 14:51:39 +0200

    added html client for testing

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ var port = process.env.PORT || process.env.npm_package_config_port;
 var app = express();
 var i = 0;
 
-app.get('/', function(req, res) {
-	res.send('use<ul><li><a href="/inc">to increase i</a></li>' +
-           '<li><a href="/exit">to exit</a></li></ul>');
-});
+app.use(bodyParser.json());
+app.use(express.static(__dirname));
 
 app.get('/inc', function(req, res) {
   i++;
-	res.send('hello world ' + i + ' times from port ' + port);
+  res.send({ "i": i });
 });
 
 app.get('/exit', function(req, res) {
