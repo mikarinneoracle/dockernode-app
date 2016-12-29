5a4d693e56c07bc053e40a4b91837f675d772f88
Mika Rinne <mika.rinne@oracle.com>
2016-12-29 12:22:18 +0200

    configuring port via package.json, works in Docker

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var port = 3000;
+var port = process.env.PORT || process.env.npm_package_config_port;
 var app = express();
+var i = 0;
 
 app.get('/', function(req, res) {
-	res.send('hello world');
+  i++;
+	res.send('hello world ' + i + ' times from port ' + port);
 });
 
 app.listen(port, function() {
