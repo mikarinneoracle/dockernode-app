647a6e5d0aaa8f222386cc568707f245a72f8340
Mika Rinne <mika.rinne@oracle.com>
2016-12-29 13:08:37 +0200

    url commands added to increase i or exit process

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ var app = express();
 var i = 0;
 
 app.get('/', function(req, res) {
+	res.send('use<ul><li><a href="/inc">to increase i</a></li><li><a href="/exit">to exit</a></li></ul>');
+});
+
+app.get('/inc', function(req, res) {
   i++;
 	res.send('hello world ' + i + ' times from port ' + port);
+});
+
+app.get('/exit', function(req, res) {
+  process.exit();
 });
 
 app.listen(port, function() {
