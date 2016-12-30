7cedf62b29729ebafa16682cdc984144b4d806a3
Mika Rinne <mika.rinne@oracle.com>
2016-12-30 10:57:18 +0200

    session context added

--- server.js
+++ server.js
@@ -2,14 +2,25 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var port = process.env.PORT || process.env.npm_package_config_port;
 var app = express();
-var i = 0;
+var session = require('express-session');
+
+// Use the session middleware
+app.use(session(
+  { secret: 'keyboard cat', resave: false, saveUninitialized: true, cookie: { maxAge: 60000 }}
+));
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
 
 app.get('/inc', function(req, res) {
-  i++;
-  res.send({ "i": i });
+  var session = req.session;
+  if (session.i) {
+    session.i++;
+  } else {
+    session.i = 1;
+  }
+  console.log(session.i);
+  res.send({ "i": session.i });
 });
 
 app.get('/exit', function(req, res) {
