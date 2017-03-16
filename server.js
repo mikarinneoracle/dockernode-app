b905d6cc0ece1761751952dd681e39a5c44ae3c8
Mika Rinne <mika.rinne@oracle.com>
2017-03-16 11:46:03 +0200

    logic made simpler

--- server.js
+++ server.js
@@ -57,48 +57,6 @@ app.get('/env/', function(req, res) {
 
 /**
  * @swagger
- * /db:
- *   get:
- *     tags:
- *       - DB
- *     description: Logs on to MondoDB instance. If succesfull writes a log row and returns the number of rows.
- *
- *     produces:
- *       - application/json
- *     responses:
- *       200:
- *         description: Returns status if the logon was succesfull.
- */
-
-app.get('/db/', function(req, res) {
-  MongoClient.connect('mongodb://' + mongodb_host + '/log', function(err, db) {
-    if(err)
-    {
-      console.log(err);
-      res.send({ 'status' : false, 'host' : mongodb_host, 'error' : err });
-    } else {
-      var collection = db.collection('log');
-      collection.find({}).toArray(function(err, log) {
-        if(err)
-        {
-            console.log(err);
-        }
-        var row = {'datetime' : new Date() };
-      	collection.insert(row, function(err, r) {
-      		if(err)
-      		{
-      				console.log(err);
-      		}
-      	});
-        res.send({ 'status' : true, 'log size' : log.length + 1 });
-        db.close();
-      });
-    }
-  });
-});
-
-/**
- * @swagger
  * /inc:
  *   get:
  *     tags:
@@ -129,67 +87,65 @@ app.get('/db/', function(req, res) {
 app.get('/inc/', function(req, res) {
   var userid = req.query.userid; // Optional
   var result;
-  if(useSessions)
+  if(userid && mongodb_host)
   {
-    if(userid && mongodb_host)
-    {
-      // Try locating session from db, create if not found
-      MongoClient.connect('mongodb://' + mongodb_host + '/sessions', function(err, db) {
-        if(err)
-        {
-          console.log(err);
-          res.send({ 'i': -1 , 'err': err.message });
-          return;
-        } else {
-          var collection = db.collection('sessions');
-          collection.find({ userid : userid }).toArray(function(err, sessions) {
-            if(err)
+    // Try locating session from db, create if not found
+    MongoClient.connect('mongodb://' + mongodb_host + '/sessions', function(err, db) {
+      if(err)
+      {
+        console.log(err);
+        res.send({ 'i': -1 , 'err': err.message });
+        return;
+      } else {
+        var collection = db.collection('sessions');
+        collection.find({ userid : userid }).toArray(function(err, sessions) {
+          if(err)
+          {
+              console.log(err);
+              res.send({ 'i': -1 , 'err': err.message });
+              return;
+          } else
+          {
+            if(sessions && sessions.length == 1)
             {
-                console.log(err);
-                res.send({ 'i': -1 , 'err': err.message });
-                return;
-            } else
-            {
-              if(sessions && sessions.length == 1)
-              {
-                  var userid_i = sessions[0].i;
-                  var _id = sessions[0]._id;
-                  userid_i++;
-                  // update
-                  console.log("Updating db session " + (userid ? userid : ''));
-                  var session = {'userid' : userid, 'i' :  userid_i};
-                  collection.update({ userid : userid }, session, function(err, r) {
-                    if(err)
-                    {
-                        console.log(err);
-                        res.send({ 'i': -1 , 'err': err.message });
-                        return;
-                    }
-                    res.send({ 'i': userid_i , 'useSessions': useSessions, 'userid':userid });
-                    db.close();
-                    return;
-                  });
-              } else {
-                  // insert
-                  console.log("Creating a new db session " + (userid ? userid : ''));
-                  var session = {'userid' : userid, 'i' :  0};
-                  collection.insert(session, function(err, r) {
-                    if(err)
-                    {
-                        console.log(err);
-                        res.send({ 'i': -1 , 'err': err.message });
-                        return;
-                    }
-                    res.send({ 'i': 0 , 'useSessions': useSessions, 'userid':userid });
-                    db.close();
-                    return;
-                  });
-              }
+                var userid_i = sessions[0].i;
+                var _id = sessions[0]._id;
+                userid_i++;
+                // update
+                console.log("Updating db session " + (userid ? userid : ''));
+                var session = {'userid' : userid, 'i' :  userid_i};
+                collection.update({ userid : userid }, session, function(err, r) {
+                  if(err)
+                  {
+                      console.log(err);
+                      res.send({ 'i': -1 , 'err': err.message });
+                      return;
+                  }
+                  res.send({ 'i': userid_i , 'useSessions': useSessions, 'userid':userid });
+                  db.close();
+                  return;
+                });
+            } else {
+                // insert
+                console.log("Creating a new db session " + (userid ? userid : ''));
+                var session = {'userid' : userid, 'i' :  0};
+                collection.insert(session, function(err, r) {
+                  if(err)
+                  {
+                      console.log(err);
+                      res.send({ 'i': -1 , 'err': err.message });
+                      return;
+                  }
+                  res.send({ 'i': 0 , 'useSessions': useSessions, 'userid':userid });
+                  db.close();
+                  return;
+                });
             }
-          });
-        }
-      });
-    } else {
+          }
+        });
+      }
+    });
+  } else if(useSessions) {
       var session = req.session;
       if (session.i != null)
       {
@@ -201,7 +157,6 @@ app.get('/inc/', function(req, res) {
       req.session.save(); // Unless calling this the session is saved in the end of the req by default
       result = session.i;
       res.send({ 'i': result , 'useSessions': useSessions });
-    }
   } else {
     result = i++;
     res.send({ 'i': i , 'useSessions': useSessions });
@@ -245,44 +200,41 @@ app.get('/inc/', function(req, res) {
 app.get('/last/', function(req, res) {
   var userid = req.query.userid; // Optional
   var result;
-  if(useSessions)
+  if(userid && mongodb_host)
   {
-    if(userid && mongodb_host)
-    {
-      MongoClient.connect('mongodb://' + mongodb_host + '/sessions', function(err, db) {
-        if(err)
-        {
-          console.log(err);
-          res.send({ 'i': -1 , 'err': err.message });
-          return;
-        } else {
-          var collection = db.collection('sessions');
-          collection.find({ userid : userid }).toArray(function(err, sessions) {
-            if(err)
+    MongoClient.connect('mongodb://' + mongodb_host + '/sessions', function(err, db) {
+      if(err)
+      {
+        console.log(err);
+        res.send({ 'i': -1 , 'err': err.message });
+        return;
+      } else {
+        var collection = db.collection('sessions');
+        collection.find({ userid : userid }).toArray(function(err, sessions) {
+          if(err)
+          {
+              console.log(err);
+              res.send({ 'i': -1 , 'err': err.message });
+              return;
+          } else
+          {
+            if(sessions && sessions.length == 1)
             {
-                console.log(err);
-                res.send({ 'i': -1 , 'err': err.message });
+                var userid_i = sessions[0].i;
+                res.send({ 'i': userid_i , 'useSessions': useSessions, 'userid':userid });
+                db.close();
                 return;
-            } else
-            {
-              if(sessions && sessions.length == 1)
-              {
-                  var userid_i = sessions[0].i;
-                  res.send({ 'i': userid_i , 'useSessions': useSessions, 'userid':userid });
-                  db.close();
-                  return;
-              } else {
-                  // Not found
-                  db.close();
-                  return res.status(404).json( { 'msg' : userid + ' not found' });
-              }
+            } else {
+                // Not found
+                db.close();
+                return res.status(404).json( { 'msg' : userid + ' not found' });
             }
-          });
-        }
-      });
-    } else {
-      return res.status(500).json( { 'msg' : 'Need to have db configured and userid supplied in this service call.' });
-    }
+          }
+        });
+      }
+    });
+  } else {
+    return res.status(500).json( { 'msg' : 'Need to have db configured and userid supplied in this service call.' });
   }
 });
 
