3b69602586fd4242dc846b9fba2dbf991700ed29
Mika Rinne <mika.rinne@oracle.com>
2017-05-23 11:05:59 +0300

    Loader.io key support

--- rollingdeploy.sh
+++ rollingdeploy.sh
@@ -22,7 +22,7 @@ POSTDATA=$(cat <<ENDOFTEMPLATE
     "app": ${SCALE_AMOUNT}
   },
   "stack": {
-    "content": "version: 2\nservices:\n  app:\n    image: \"${DOCKER_REGISTRY}/${IMAGE_NAME}:${TRAVIS_BUILD_NUMBER}\"\n    ports:\n      - ${EXPOSED_PORT}/tcp\n    environment:\n      - \LOADERIO_KEY=loaderio-0806b2c02d2b955d22996f2650fc117a\n      - \"occs:availability=per-pool\"\n      - \"occs:scheduler=random\"\n",
+    "content": "version: 2\nservices:\n  app:\n    image: \"${DOCKER_REGISTRY}/${IMAGE_NAME}:${TRAVIS_BUILD_NUMBER}\"\n    ports:\n      - ${EXPOSED_PORT}/tcp\n    environment:\n      - LOADERIO_KEY=loaderio-0806b2c02d2b955d22996f2650fc117a\n      - \"occs:availability=per-pool\"\n      - \"occs:scheduler=random\"\n",
     "service_id": "app",
     "service_name": "${APP_FRIENDLY_NAME} ${TRAVIS_BUILD_NUMBER}",
     "subtype": "service"
